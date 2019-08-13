778d2fcd80d45505981220db54e04a408e4311ec
leosoftleo <leosoftleo@gmail.com>
2019-08-13 15:17:15 +0800

    Create home page and news

---
+++ lib/module/home/view/VHome.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+          backgroundColor: Color(0xFFFFFFFF),
+          body: TabBarView(
+            children: [
+              Container(
+                child: Container(),
+              ),
+              Container(
+                child: Container(),
+              ),
+              Container(
+                child: Container(),
+              ),
+              Container(
+                child: Container(),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(width: 0.3, color: Color(0xFFAAAAAA)),
+              ),
+            ),
+            child: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.explore),
+                ),
+                Tab(
+                  icon: Icon(Icons.whatshot),
+                ),
+                Tab(
+                  icon: Icon(Icons.search),
+                ),
+                Tab(
+                  icon: Icon(Icons.person_outline),
+                )
+              ],
+              labelColor: Color(0xFF3361CA),
+              unselectedLabelColor: Colors.grey,
+              indicatorColor: Color(0xFF3361CA),
+            ),
+          )),
+    );
+  }
+}
